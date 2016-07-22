58d52a884348c60f5793710e00f29af00c058ee1
qiyutao <qiyutao12@163.com>
2016-07-22 16:07:07 +0800

    MapView's use

---
+++ app/src/main/java/com/seven/baidumapdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.seven.baidumapdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+}
